5fef7fa62a5bb88eeefb0eb71bb3bea43ad81f5b
Sandeeb Adhikari <sa2734@njit.edu>
2024-11-10 00:55:53 -0500

    change on next.config.ts3 file

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  redirects: async () => [
+    {
+      source: "/",
+      destination: "/sign_in",
+      permanent: true,
+    },
+  ],
+  experimental: {
+    appDir: true,
+  },
 };
 
 export default nextConfig;
